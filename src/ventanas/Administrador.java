4948c29c5ef9e3fc523bf5aa9f52cc26e5e435ad
Andres Felipe <91640921+AndresGratus@users.noreply.github.com>
2021-12-20 01:19:56 -0500

    Cambiamos el cierre del administrador para poder regresar al login

--- src/ventanas/Administrador.java
+++ src/ventanas/Administrador.java
@@ -24,7 +24,7 @@ public class Administrador extends javax.swing.JFrame {
         setTitle("Administrador");
         setLocationRelativeTo(null);
         
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         
         ImageIcon wallpaper = new ImageIcon("src/imagenes/wallpaperPrincipal.jpg");
         Icon icono = new ImageIcon(wallpaper.getImage().getScaledInstance(jLabel_Wallpaper.getWidth(), jLabel_Wallpaper.getHeight(), Image.SCALE_DEFAULT));
