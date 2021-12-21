1526dd7633fe77089227ad26d693c62acf4d6515
Andres Felipe <91640921+AndresGratus@users.noreply.github.com>
2021-12-21 03:34:25 -0500

    Realizamos un clase metodo donde guardaremos las preguntas y la sacaremos al azar dependiendo de la ronda;

--- src/sofka/Sofka.java
+++ src/sofka/Sofka.java
@@ -10,24 +10,17 @@ package sofka;
  * @author Andres
  */
 //Comentario
-
+import ventanas.Metodos;
+import ventanas.PreguntaDate;
 public class Sofka {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int ID=2;
-        String ronda="";
-        
-        if (ID >=0 && ID <=5) {
-            ronda = "Ronda 1";
-        } else if (ID >= 6 && ID <=10) {
-            ronda = "Ronda 2";
-        } else if (ID >= 11 && ID <=15) {
-            ronda ="Ronda 3";
-        }
-        System.out.println(ronda);
+        Metodos obj = new Metodos();
+        obj.ingresarArreglo();
+        obj.mostra();
     }
     
 }
