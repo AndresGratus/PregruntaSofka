b6ac7083429af0641efefab321389f9d5ec9e1e9
Andres Felipe <91640921+AndresGratus@users.noreply.github.com>
2021-12-21 03:50:36 -0500

    Llenamos los metodos con la informacionde la bd_preg

--- src/ventanas/Metodos.java
+++ src/ventanas/Metodos.java
@@ -14,7 +14,7 @@ public class Metodos {
 
     int id;
     String pregunta, respuesta, falsaUno, falsados, falsaTres, categoria, ronda;
-    String text ="";
+    String text = "";
 
     public Metodos() {
         RondaUno = new PreguntaDate[5];
@@ -24,48 +24,106 @@ public class Metodos {
         RondaCinco = new PreguntaDate[5];
     }
 
-    public void ingresarArreglo() {
-        //Variable de prueba 
-        int aux =1;
-        for (int i = 0; i < 5; i++) {
-            try {
-                Connection conec = Conexiones.connect();
-                PreparedStatement pst = conec.prepareCall(
-                        "select * from preguntas where id_pregunta ='" + aux + "'");
-                ResultSet rs = pst.executeQuery();
+    public void ingresarArreglo(int n) {
 
-                
-                if (rs.next()) {
-                    id = rs.getInt("id_pregunta");
-                    pregunta = rs.getString("pregunta");
-                    respuesta = rs.getString("op_verdadera");
-                    falsaUno = rs.getString("op_falsa_uno");
-                    falsados = rs.getString("op_falsa_dos");
-                    falsaTres = rs.getString("op_falsa_tres");
-                    categoria = rs.getString("categoria");
-                    ronda = rs.getString("ronda");
+        if (n == 1) {
+            int aux = 1;
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Connection conec = Conexiones.connect();
+                    PreparedStatement pst = conec.prepareCall(
+                            "select * from preguntas where id_pregunta ='" + aux + "'");
+                    ResultSet rs = pst.executeQuery();
+
+                    if (rs.next()) {
+                        id = rs.getInt("id_pregunta");
+                        pregunta = rs.getString("pregunta");
+                        respuesta = rs.getString("op_verdadera");
+                        falsaUno = rs.getString("op_falsa_uno");
+                        falsados = rs.getString("op_falsa_dos");
+                        falsaTres = rs.getString("op_falsa_tres");
+                        categoria = rs.getString("categoria");
+                        ronda = rs.getString("ronda");
+                    }
+                    conec.close();
+                } catch (SQLException e) {
+                    System.err.println("Error en cargar usuario. " + e);
+                    JOptionPane.showMessageDialog(null, "Error al cargar contacte al Administrador. ");
                 }
-                conec.close();
-            } catch (SQLException e) {
-                System.err.println("Error en cargar usuario. " + e);
-                JOptionPane.showMessageDialog(null, "Error al cargar contacte al Administrador. ");
+
+                RondaUno[i] = new PreguntaDate(id, pregunta, ronda, falsaUno, falsaUno, falsaTres, categoria, ronda);
+                aux++;
             }
-            
-            RondaUno[i] = new PreguntaDate(id, pregunta, ronda, falsaUno, falsaUno, falsaTres, categoria, ronda);
-            aux++;
+
+        } else if (n == 2) {
+            int aux = 6;
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Connection conec = Conexiones.connect();
+                    PreparedStatement pst = conec.prepareCall(
+                            "select * from preguntas where id_pregunta ='" + aux + "'");
+                    ResultSet rs = pst.executeQuery();
+
+                    if (rs.next()) {
+                        id = rs.getInt("id_pregunta");
+                        pregunta = rs.getString("pregunta");
+                        respuesta = rs.getString("op_verdadera");
+                        falsaUno = rs.getString("op_falsa_uno");
+                        falsados = rs.getString("op_falsa_dos");
+                        falsaTres = rs.getString("op_falsa_tres");
+                        categoria = rs.getString("categoria");
+                        ronda = rs.getString("ronda");
+                    }
+                    conec.close();
+                } catch (SQLException e) {
+                    System.err.println("Error en cargar usuario. " + e);
+                    JOptionPane.showMessageDialog(null, "Error al cargar contacte al Administrador. ");
+                }
+
+                RondaDos[i] = new PreguntaDate(id, pregunta, ronda, falsaUno, falsaUno, falsaTres, categoria, ronda);
+                aux++;
+            }
+        } else if (n == 3) {
+            int aux = 11;
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Connection conec = Conexiones.connect();
+                    PreparedStatement pst = conec.prepareCall(
+                            "select * from preguntas where id_pregunta ='" + aux + "'");
+                    ResultSet rs = pst.executeQuery();
+
+                    if (rs.next()) {
+                        id = rs.getInt("id_pregunta");
+                        pregunta = rs.getString("pregunta");
+                        respuesta = rs.getString("op_verdadera");
+                        falsaUno = rs.getString("op_falsa_uno");
+                        falsados = rs.getString("op_falsa_dos");
+                        falsaTres = rs.getString("op_falsa_tres");
+                        categoria = rs.getString("categoria");
+                        ronda = rs.getString("ronda");
+                    }
+                    conec.close();
+                } catch (SQLException e) {
+                    System.err.println("Error en cargar usuario. " + e);
+                    JOptionPane.showMessageDialog(null, "Error al cargar contacte al Administrador. ");
+                }
+
+                RondaTres[i] = new PreguntaDate(id, pregunta, ronda, falsaUno, falsaUno, falsaTres, categoria, ronda);
+                aux++;
+            }
         }
+        //Variable de prueba 
 
     }
-    
-    public void mostra(){
-        
+
+    public void mostra() {
+
         for (int i = 0; i < RondaUno.length; i++) {
             PreguntaDate preguntaDate = RondaUno[i];
-            text += (1+i) + "Id pregunta" + preguntaDate.getCont() + " -  pregunta" + preguntaDate.getPregunta();
+            text += (1 + i) + "Id pregunta" + preguntaDate.getCont() + " -  pregunta" + preguntaDate.getPregunta();
         }
-        
+
         System.out.println(" " + text);
     }
-    
 
 }
